2379ed9f9d985f149334ab18ac5fc868eb0bebe5
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-27 01:10:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-
+event.preventDefault();
+let firstName = document.getElementsByName('fname')[0].value;
+let lastName = document.getElementsByName('lname')[0].value;
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+let fullName = firstName + ' ' + lastName;
+window.alert(fullName);
 }
